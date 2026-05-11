7aafc0bd8ac02872a9cf93fb55d77a668d156a16
baiyi <baiyitongxue@qq.com>
2026-05-11 21:15:40 +0800

    fix(core): 更新 API 文档版本和配置
    - 更新 swagger 文档版本号至 1.0.4
    - 添加 HTTP schemes 配置项
    - 调整 docs.go 中版本和协议字段

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,22 @@
 package main
 
-import "atelino/internal/app"
-
-//	@title			Atelino API
-//	@version		1.0.4
-//	@description	Atelino 后端 API 文档
-
-//	@host		localhost:8080
-//	@BasePath	/
-// @schemes http
-//	@securityDefinitions.apikey	BearerAuth
-//	@in							header
-//	@name						Authorization
-
-//	@externalDocs.description	OpenAPI
-//	@externalDocs.url			https://swagger.io/resources/open-api/
 import (
+	"atelino/internal/app"
+
+	//	@title			Atelino API
+	//	@version		1.0.4
+	//	@description	Atelino 后端 API 文档
+
+	//	@host						localhost:8080
+	//	@BasePath					/
+	//	@schemes					http
+	//	@securityDefinitions.apikey	BearerAuth
+	//	@in							header
+	//	@name						Authorization
+
+	//	@externalDocs.description	OpenAPI
+	//	@externalDocs.url			https://swagger.io/resources/open-api/
+
 	_ "atelino/pkg/docs"
 )
 
--- pkg/docs/docs.go
+++ pkg/docs/docs.go
@@ -628,10 +628,10 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0.3",
+	Version:          "1.0.4",
 	Host:             "localhost:8080",
 	BasePath:         "/",
-	Schemes:          []string{},
+	Schemes:          []string{"http"},
 	Title:            "Atelino API",
 	Description:      "Atelino 后端 API 文档",
 	InfoInstanceName: "swagger",
