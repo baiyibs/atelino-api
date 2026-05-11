418438cb71f7007d7f439c31e413090eb4ea67d6
baiyi <baiyitongxue@qq.com>
2026-05-11 21:15:03 +0800

    fix(core): 更新 API 文档版本至 v1.0.4
    - 将 Swagger 注解中的 API 版本从 v1.0.3 升级到 v1.0.4

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,14 @@
 package main
 
+import "atelino/internal/app"
+
 //	@title			Atelino API
-//	@version		1.0.3
+//	@version		1.0.4
 //	@description	Atelino 后端 API 文档
 
 //	@host		localhost:8080
 //	@BasePath	/
-
+// @schemes http
 //	@securityDefinitions.apikey	BearerAuth
 //	@in							header
 //	@name						Authorization
@@ -14,7 +16,6 @@ package main
 //	@externalDocs.description	OpenAPI
 //	@externalDocs.url			https://swagger.io/resources/open-api/
 import (
-	"atelino/internal/app"
 	_ "atelino/pkg/docs"
 )
 
